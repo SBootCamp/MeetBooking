c9f3509d60c77b14ddc95036cf3265647eece5a5
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-28 13:22:19 +0300

    test_commit

---
+++ MeetBooking/celery.py
@@ -0,0 +1,16 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_project.settings')
+
+app = Celery('send_email')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'send-spam': {
+        'task': 'app.task.send_spam_email',
+        'schedule': crontab(minute='*/30')
+    }
+}
--- MeetBooking/settings.py
+++ MeetBooking/settings.py
@@ -89,3 +89,23 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#REDIS
+REDIS_HOST = '0.0.0.0'
+REDIS_PORT = '6379'
+CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+
+#SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = 'test-infomaxim@yandex.ru'
+EMAIL_HOST_PASSWORD = 'UMz-ALG-aXq-8cv'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
