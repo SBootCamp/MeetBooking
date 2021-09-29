052e13519aed56804b01224e80e73204e31890fc
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-29 15:09:11 +0300

    celery_draft

--- booking/tasks.py
+++ booking/tasks.py
@@ -1,16 +1,16 @@
 from django.core.mail import send_mail
 from booking.celery import app
+from booking.models import Event
+from datetime import datetime
 
 
 @app.task
 def send_spam_email():
-    send_mail(
-        'Тест',
-        'Тест',
-        'mbannakov13@gmail.com',
-        ['mbannakov14@yahoo.com'],
-        fail_silently=False,
-    )
+    #now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    #list = Event.objects.values_list('start_time')
+    #print(list[0])
+
 
 #celery -A MeetBooking worker -l info
 #celery -A MeetBooking beat -l info
+#from booking.tasks import send_spam_email
