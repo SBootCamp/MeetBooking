2dcab957adb25b5056601565559b79774306a2cc
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-29 17:09:54 +0300

    flake8

--- booking/models.py
+++ booking/models.py
@@ -10,11 +10,11 @@ from booking.contrib.postgres.functions import TsTzRange
 
 class Cabinet(models.Model):
     # photo = models.ImageField('Фото кабинета', upload_to='static/')
-    place_count = models.IntegerField(verbose_name = 'Всего мест', default=0)
-    projector = models.BooleanField(verbose_name = 'Наличие проектора', default=False)
-    tv = models.BooleanField(verbose_name = 'Наличие ТВ', default=False)
-    floor = models.IntegerField(verbose_name = 'Этаж')
-    room_number = models.IntegerField(verbose_name = 'Номер кабинета')
+    place_count = models.IntegerField(verbose_name='Всего мест', default=0)
+    projector = models.BooleanField(verbose_name='Наличие проектора', default=False)
+    tv = models.BooleanField(verbose_name='Наличие ТВ', default=False)
+    floor = models.IntegerField(verbose_name='Этаж')
+    room_number = models.IntegerField(verbose_name='Номер кабинета')
 
     def __str__(self):
         return str(self.room_number)
@@ -25,26 +25,26 @@ class Cabinet(models.Model):
 
 
 class Event(models.Model):
-    title = models.CharField(verbose_name = 'Название мероприятия', max_length=250, default='')
-    start_time = models.DateTimeField(verbose_name = 'Время начала мероприятия')
-    end_time = models.DateTimeField(verbose_name = 'Время окончания мероприятия')
+    title = models.CharField(verbose_name='Название мероприятия', max_length=250, default='')
+    start_time = models.DateTimeField(verbose_name='Время начала мероприятия')
+    end_time = models.DateTimeField(verbose_name='Время окончания мероприятия')
     cabinet = models.ForeignKey(
         Cabinet,
         on_delete=models.PROTECT,
-        related_name = 'event_cabinet',
-        verbose_name = 'Номер кабинета'
+        related_name='event_cabinet',
+        verbose_name='Номер кабинета'
     )
     visitors = models.ManyToManyField(
         User,
-        verbose_name = 'Посетители',
-        related_name = 'event_visitors',
+        verbose_name='Посетители',
+        related_name='event_visitors',
         blank=True,
     )
     owner = models.ForeignKey(
         User,
         on_delete=models.PROTECT,
-        verbose_name = 'Организатор',
-        related_name = 'event_owner'
+        verbose_name='Организатор',
+        related_name='event_owner'
     )
 
     # TODO:add functionality
@@ -71,14 +71,14 @@ class Event(models.Model):
         verbose_name_plural = 'Мероприятия'
         constraints = [
             ExclusionConstraint(
-                name = 'overlap_booking',
-                expressions = (
+                name='overlap_booking',
+                expressions=(
                     (TsTzRange('start_time', 'end_time', RangeBoundary()), RangeOperators.OVERLAPS),
                     ('cabinet', RangeOperators.EQUAL),
                 ),
             ),
             models.CheckConstraint(
-                name = 'check_datetime',
-                check=models.Q(start_time__lt = models.F("end_time"))
+                name='check_datetime',
+                check=models.Q(start_time__lt=models.F("end_time"))
             ),
         ]
