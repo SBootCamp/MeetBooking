651e0887fcecee2e34613c0a03a04a2442bb6499
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-29 18:10:46 +0300

    celery_draft

--- booking/tasks.py
+++ booking/tasks.py
@@ -1,15 +1,27 @@
 from django.core.mail import send_mail
 from booking.celery import app
 from booking.models import Event
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 @app.task
 def send_spam_email():
-    #now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    #list = Event.objects.values_list('start_time')
-    #print(list[0])
-
+    now_time = datetime.now().strftime('%Y-%m-%d %H:%M')
+    list = Event.objects.order_by('start_time')
+    nearest_event_time = getattr(list[0], 'start_time')
+    print(nearest_event_time.strftime('%Y-%m-%d %H:%M'))
+    print(now_time)
+    print('------')
+    time_to_send = nearest_event_time - timedelta(minutes=30)
+    print(time_to_send.strftime('%Y-%m-%d %H:%M'))
+    if time_to_send.strftime('%Y-%m-%d %H:%M') == nearest_event_time.strftime('%Y-%m-%d %H:%M'):
+        send_mail(
+            'Напоминание о мероприятии',
+            '',
+            'mbannakov13@gmail.com',
+            [mail.email],
+            fail_silently=False,
+        )
 
 #celery -A MeetBooking worker -l info
 #celery -A MeetBooking beat -l info
