2e621406ddd3bd386178e205e5cd9bf69c953b68
dmitriy <krivosheind28@gmail.com>
2021-10-04 18:28:40 +0300

    add validation in EventCreateUpdateSerializer

--- booking/services.py
+++ booking/services.py
@@ -27,19 +27,24 @@ def create_datetime_dict(dates: list, times: list) -> typing.Dict[str, typing.Di
     return {str(date.date()): {str(datetime.time(*time)): None for time in times} for date in dates}
 
 
+def create_datetime_list(dates: list, times: list) -> typing.List[datetime.datetime]:
+    return [date + datetime.timedelta(hours=time[0], minutes=time[1]) for date in dates for time in times]
+
+
 TIMES = create_time_list(START_TIME, END_TIME, STEP_TIME)
 DATES = create_date_list(YEAR_NOW, MONTH_NOW)
-DATE_TIMES = create_datetime_dict(DATES, TIMES)
+DATE_TIMES_DICT = create_datetime_dict(DATES, TIMES)
+DATE_TIMES_LIST = create_datetime_list(DATES, TIMES)
 
 
-def create_schedule(events: list) -> typing.Dict[str, typing.Dict[str, None or dict]]:
-    dict_output = copy.deepcopy(DATE_TIMES)
+def create_schedule(events: typing.List[dict]) -> typing.Dict[str, typing.Dict[str, None or dict]]:
+    schedule = copy.deepcopy(DATE_TIMES_DICT)
     for event in events:
         start_time = event['start_time']
         end_time = event['end_time']
         while start_time != end_time:
             date = str(start_time.date())
             time = str(start_time.time())
-            dict_output[date][time] = event
+            schedule[date][time] = event
             start_time += datetime.timedelta(minutes=STEP_TIME)
-    return dict_output
+    return schedule
