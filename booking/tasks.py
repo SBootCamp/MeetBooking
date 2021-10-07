78a666a04ce893c57aea91b5e8e248e46972055a
TheFeenStudio <mbannakov14@yahoo.com>
2021-10-07 14:06:21 +0300

    review changes

--- booking/tasks.py
+++ booking/tasks.py
@@ -1,9 +1,7 @@
 from zoneinfo import ZoneInfo
 
 from django.template.loader import render_to_string
-from django.core.mail import send_mass_mail, send_mail
-from django.contrib.auth.models import User
-from django.db.models import Prefetch
+from django.core.mail import send_mail
 
 from django.conf import settings
 
@@ -12,43 +10,56 @@ from datetime import timedelta
 from django.utils import timezone
 from booking.models import Event
 
-
-# from booking.tasks import send_events_mail
 @app.task
 def send_events_mail():
     now_date = timezone.now()
     end = timezone.now() + timedelta(minutes=settings.STEP_TIME_MINUTES)
 
-    event_list = Event.objects.prefetch_related(Prefetch('visitors', queryset=User.objects.only('email'))) \
-        .filter(start_time__gt=now_date, start_time__lte=end) \
+    event_list = Event.objects.filter(start_time__gt=now_date, start_time__lte=end) \
         .select_related('cabinet',
                         'owner',
                         'cabinet')
 
-    print(event_list)
     for event in event_list:
-        # email_list = list(event.visitors.values_list('email', flat=True))
-        # message = ('Напоминание о мероприятии',
-        #            render_to_string('visitors_message.html'),
-        #
-        #            settings.EMAIL_HOST_USER,
-        #            email_list)
-        #
-        # send_mass_mail((message,))
+        email_list = list(event.visitors.values_list('email', flat=True))
+        username_list = list(event.visitors.values_list('username', flat=True))
 
-        owner_message = render_to_string(
-            'owner_message.html',
+        visitors_message = render_to_string(
+            'visitors_message.html',
             {
-                'event_start_time': event.start_time,
-                'event_end_time': event.end_time,
+                'event_start_time': event.start_time.astimezone(tz=ZoneInfo('Europe/Moscow')),
+                'event_end_time': event.end_time.astimezone(tz=ZoneInfo('Europe/Moscow')),
                 'event_title': event.title,
                 'event_cabinet': event.cabinet,
                 'event_cabinet_floor': event.cabinet.floor,
             }
         )
 
+
+        # Письма для посетителей мероприятия
+        send_mail('Напоминание о начале мероприятия',
+                  'Через 30 минут начало мероприятия',
+                  settings.EMAIL_HOST_USER,
+                  email_list,
+                  html_message=visitors_message)
+
+
+        owner_message = render_to_string(
+            'owner_message.html',
+            {
+                'user_name_list': username_list,
+                'event_start_time': event.start_time.astimezone(tz=ZoneInfo('Europe/Moscow')),
+                'event_end_time': event.end_time.astimezone(tz=ZoneInfo('Europe/Moscow')),
+                'event_title': event.title,
+                'event_cabinet': event.cabinet,
+                'event_cabinet_floor': event.cabinet.floor,
+            }
+        )
+
+
+        # Письмо для руководителя
         send_mail(
-            'Напоминание о начале мероприятия',
+            'Вы создали мероприятие которое начнется через 30 минут',
             'Через 30 минут начало мероприятия',
             settings.EMAIL_HOST_USER,
             [event.owner.email],
