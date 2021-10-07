5e1002d67f2557357d0cdf31d39aecd7cd214f7b
TheFeenStudio <mbannakov14@yahoo.com>
2021-10-07 17:20:33 +0300

    Добавил изменение timezone внутри шаблона отправки письма

--- booking/tasks.py
+++ booking/tasks.py
@@ -1,6 +1,7 @@
 from zoneinfo import ZoneInfo
 
 from django.contrib.auth.models import User
+from django.db.models import Prefetch
 from django.template.loader import render_to_string
 from django.core.mail import send_mail
 
@@ -11,13 +12,12 @@ from datetime import timedelta
 from django.utils import timezone
 from booking.models import Event
 
-
 @app.task
 def send_events_mail():
     now_date = timezone.now()
     end = timezone.now() + timedelta(minutes=settings.STEP_TIME_MINUTES)
 
-    event_list = Event.objects.Prefetch('visitors', queryset=User.objects.only('email', 'username'))\
+    event_list = Event.objects.prefetch_related(Prefetch('visitors', queryset=User.objects.only('email', 'username')))\
         .filter(start_time__gt=now_date, start_time__lte=end) \
         .select_related('cabinet',
                         'owner', )
@@ -25,12 +25,11 @@ def send_events_mail():
     for event in event_list:
         email_list = [visitors.email for visitors in event.visitors.all()]
         username_list = [visitors.username for visitors in event.visitors.all()]
-
         visitors_message = render_to_string(
             'visitors_message.html',
             {
-                'event_start_time': event.start_time.astimezone(tz=ZoneInfo('Europe/Moscow')),
-                'event_end_time': event.end_time.astimezone(tz=ZoneInfo('Europe/Moscow')),
+                'event_start_time': event.start_time,
+                'event_end_time': event.end_time,
                 'event_title': event.title,
                 'event_cabinet': event.cabinet,
                 'event_cabinet_floor': event.cabinet.floor,
