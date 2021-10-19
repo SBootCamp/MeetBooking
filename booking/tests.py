f7c96a72cdfe1e0da303e366df6cca409ccdac71
dmitriy <krivosheind28@gmail.com>
2021-10-19 16:48:16 +0300

    add factories and change tests

--- booking/tests.py
+++ booking/tests.py
@@ -9,78 +9,65 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase, APIClient
 
-from .factories import UserFactory, CabinetFactory, EventFactory
+from .factories import UserFactory, CabinetFactory, EventFactory, get_datetime, create_event_json
 from .models import Event, Cabinet
 
 
 class BookingTests(APITestCase):
-    def create_event_json(self, start_time: datetime, end_time: datetime, visitors=None) -> dict:
-        if visitors is None:
-            visitors = []
-        event = {
-            'title': 'tests',
-            'start_time': start_time,
-            'end_time': end_time,
-            'owner': self.user.id,
-            'cabinet': self.cabinet.id,
-            'visitors': visitors,
-        }
-        return event
-
     def setUp(self):
-        self.now = timezone.now()
         self.client = APIClient()
         self.cabinet = CabinetFactory()
-        self.user = UserFactory()
-        self.event = EventFactory()
-        self.visitors = UserFactory().create_batch(5)
-        print(self.visitors)
-        self.user_token = Token.objects.create(user=self.user)
-
+        self.user_1 = UserFactory()
+        self.user_2 = UserFactory()
+        self.event = EventFactory(cabinet=self.cabinet, owner=self.user_1)
+        self.visitors = [UserFactory().id for _ in range(10)]
+        self.user_token_1 = Token.objects.create(user=self.user_1)
+        self.user_token_2 = Token.objects.create(user=self.user_2)
         self.events_detail_url = reverse(
             'events-detail', kwargs={
-                'room_number': self.cabinet.room_number,
+                'name': self.cabinet.name,
                 'pk': self.event.id
             })
         self.events_list_url = reverse(
             'events-list', kwargs={
-                'room_number': self.cabinet.room_number,
+                'name': self.cabinet.name,
             })
 
     def test_failure_event_created(self):
-        start_time = datetime(
-            year=self.now.year,
-            month=self.now.month,
-            day=self.now.day + 1,
-            hour=10, minute=0, tzinfo=pytz.UTC
-        )
-        end_time = datetime(
-            year=self.now.year,
-            month=self.now.month,
-            day=self.now.day + 1,
-            hour=13, minute=0, tzinfo=pytz.UTC
-        )
-        event = self.create_event_json(start_time, end_time)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token.key)
+        start_time = get_datetime(hour=10)
+        end_time = get_datetime(hour=13)
+        event = create_event_json(start_time, end_time, self.cabinet, self.user_1)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token_1.key)
         response = self.client.post(self.events_list_url, event, format='json')
         self.assertEqual(response.json(), ['Указанное время занято'])
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_success_event_created(self):
-        start_time = datetime(
-            year=self.now.year,
-            month=self.now.month,
-            day=self.now.day + 1,
-            hour=15, minute=0, tzinfo=pytz.UTC
-        )
-        end_time = datetime(
-            year=self.now.year,
-            month=self.now.month,
-            day=self.now.day + 1,
-            hour=17, minute=0, tzinfo=pytz.UTC
-        )
-        # visitors =
-        event = self.create_event_json(start_time, end_time)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token.key)
+        start_time = get_datetime(hour=15)
+        end_time = get_datetime(hour=17)
+        event = create_event_json(start_time, end_time, self.cabinet, self.user_1, self.visitors)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token_1.key)
         response = self.client.post(self.events_list_url, event, format='json')
+        self.assertEqual(response.json().get('visitors'), self.visitors)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_success_patch_event(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token_1.key)
+        response = self.client.patch(self.events_detail_url, {'title': 'rename'}, format='json')
+        self.assertEqual(response.json().get('title'), 'rename')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_failure_patch_event(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token_2.key)
+        response = self.client.patch(self.events_detail_url, {'title': 'rename'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_success_delete_event(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token_1.key)
+        response = self.client.delete(self.events_detail_url, self.event.id, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_failure_delete_event(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token_2.key)
+        response = self.client.delete(self.events_detail_url, self.event.id, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
