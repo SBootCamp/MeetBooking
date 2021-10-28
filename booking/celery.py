73e3823a17da8ca1c04b9a5c6ca6870334eca99b
TheFeenStudio <mbannakov14@yahoo.com>
2021-10-28 17:32:24 +0300

    dockerignore + flake8

--- booking/celery.py
+++ booking/celery.py
@@ -2,8 +2,6 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-from django.conf import settings
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meet_booking.settings')
 
 app = Celery('booking')
